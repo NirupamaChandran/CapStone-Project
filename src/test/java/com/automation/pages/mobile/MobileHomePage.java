f07f3f1983b3f390d33492e98340c95132d32054
Nirupama.Chandran <Nirupama.Chandran@ust.com>
2024-08-30 14:34:46 +0530

    search first commit

--- src/test/java/com/automation/pages/mobile/MobileHomePage.java
+++ src/test/java/com/automation/pages/mobile/MobileHomePage.java
@@ -1,23 +1,26 @@
 package com.automation.pages.mobile;
 
 import com.automation.pages.interfaces.HomePage;
+import com.automation.utils.ConfigReader;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class MobileHomePage extends MobileBasePage implements HomePage {
 
     @FindBy(xpath = "//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]")
     WebElement goToHomePage;
+    public void openWebsite(){
+        goToHomePage.click();
+    }
+
     @FindBy(id = "com.android.permissioncontroller:id/permission_deny_button")
     WebElement denyButton;
     @FindBy(id = "android:id/button2")
     WebElement closeAdd;
     @FindBy(className = "android.widget.ImageView")
     WebElement closeButton;
-    public void openWebsite(){
-        goToHomePage.click();
-    }
-
     @Override
     public void closePopUp() {
         denyButton.click();
@@ -25,9 +28,12 @@ public class MobileHomePage extends MobileBasePage implements HomePage {
         closeButton.click();
     }
 
+
+    @FindBy(id = "com.tul.tatacliq:id/rl_tv_prominent")
+    WebElement searchBox;
     @Override
     public boolean isHomePageDisplayed() {
-        return false;
+        return searchBox.isDisplayed();
     }
 
     @Override
@@ -40,9 +46,14 @@ public class MobileHomePage extends MobileBasePage implements HomePage {
 
     }
 
+    @FindBy(xpath = "//android.widget.AutoCompleteTextView[@resource-id='android:id/search_src_text']")
+    WebElement searchInput;
     @Override
-    public void searchItem(String configValue) {
-
+    public void searchItem(String key) {
+        searchBox.click();
+        searchInput.sendKeys(ConfigReader.getConfigValue(key));
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.ENTER).pause(1000).keyUp(Keys.ENTER).build().perform();
     }
 
     @Override
