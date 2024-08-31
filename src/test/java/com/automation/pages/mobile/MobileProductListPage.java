2dc116ef3fa07cacd1916b4e1ee210b5a1c4db97
Nirupama.Chandran <Nirupama.Chandran@ust.com>
2024-08-31 12:54:05 +0530

    sort high to low

--- src/test/java/com/automation/pages/mobile/MobileProductListPage.java
+++ src/test/java/com/automation/pages/mobile/MobileProductListPage.java
@@ -2,9 +2,12 @@ package com.automation.pages.mobile;
 
 import com.automation.pages.interfaces.ProductListPage;
 import com.automation.utils.ConfigReader;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MobileProductListPage extends MobileBasePage implements ProductListPage {
@@ -13,6 +16,7 @@ public class MobileProductListPage extends MobileBasePage implements ProductList
     WebElement sortButton;
     @FindBy(id = "com.tul.tatacliq:id/textViewRefine")
     WebElement filterButton;
+
     @Override
     public boolean isProductListPageDisplayed() {
         return isDisplayed(sortButton) && isDisplayed(filterButton);
@@ -28,12 +32,13 @@ public class MobileProductListPage extends MobileBasePage implements ProductList
     List<WebElement> titleList;
     @FindBy(id = "com.tul.tatacliq:id/llGridListView")
     WebElement viewButton;
+
     @Override
     public void clickFirstProduct() {
-        if(isDisplayed(viewButton)){
+        if (isDisplayed(viewButton)) {
             viewButton.click();
         }
-        ConfigReader.setConfigValue("product.name",titleList.get(0).getText());
+        ConfigReader.setConfigValue("product.name", titleList.get(0).getText());
         titleList.get(0).click();
     }
 
@@ -44,19 +49,57 @@ public class MobileProductListPage extends MobileBasePage implements ProductList
 
     @FindBy(id = "com.tul.tatacliq:id/toolbar_icon_title")
     WebElement heading;
+
     @Override
     public boolean isItemHeadingDisplayed(String configValue) {
         return isDisplayed(heading);
     }
 
+
+    @FindBy(xpath = "//android.widget.TextView[@text='Price High to Low']")
+    WebElement highToLow;
+
     @Override
     public void sortPriceHighToLow() {
-
+        sortButton.click();
+        highToLow.click();
     }
+
+
+    @FindBy(id = "com.tul.tatacliq:id/emptyText")
+    WebElement productListEnd;
+    @FindBy(xpath = "//android.widget.LinearLayout[@resource-id='com.tul.tatacliq:id/linearLayoutPriceInfo']/android.widget.TextView[1]")
+    List<WebElement> priceList;
 
     @Override
     public boolean isPriceHighToLowSorted() {
-        return false;
+        int i = 0;
+
+        //Scroll Logic
+        Dimension dimension = driver.manage().window().getSize();
+        int width = dimension.getWidth();
+        int height = dimension.getHeight();
+
+        do {
+            List<Double> newPriceList = new ArrayList<>();
+            for (WebElement price : priceList) {
+                newPriceList.add(Double.parseDouble(price.getText().substring(1).replace(",", "")));
+            }
+            List<Double> copyPriceList = new ArrayList<>(newPriceList);
+            Collections.sort(copyPriceList);
+            Collections.reverse(copyPriceList);
+
+            if (!newPriceList.equals(copyPriceList)) {
+                return false;
+            }
+
+            scrollOrSwipe(width / 2, height / 2, width / 2, 0);
+
+            i++;
+
+        } while (i <= 5);
+
+        return true;
     }
 
     @Override
