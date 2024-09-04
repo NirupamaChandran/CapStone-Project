2982682fb4374dd900052b30bee1ae66360d5300
Nirupama.Chandran <Nirupama.Chandran@ust.com>
2024-09-04 11:56:47 +0530

    coupons mobile

--- src/test/java/com/automation/pages/mobile/MobileCartPage.java
+++ src/test/java/com/automation/pages/mobile/MobileCartPage.java
@@ -1,6 +1,8 @@
 package com.automation.pages.mobile;
 
 import com.automation.pages.interfaces.CartPage;
+import com.automation.utils.ConfigReader;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -47,6 +49,7 @@ public class MobileCartPage extends MobileBasePage implements CartPage {
         return title.getText();
     }
 
+
     @Override
     public boolean calculateTotal() {
 
@@ -54,18 +57,58 @@ public class MobileCartPage extends MobileBasePage implements CartPage {
 
     }
 
+    @FindBy(xpath = "//android.widget.TextView[@text='Total Payable']/following-sibling::android.widget.TextView")
+    WebElement totalPayable;
+    @FindBy(xpath = "//android.widget.TextView[@text='Price Details']")
+    WebElement priceDetails;
+    @FindBy(xpath = "//android.widget.TextView[@text='Check for coupon']")
+    WebElement checkForCouponBtn;
     @Override
     public void clickCheckForCoupons() {
 
+        Dimension dimension = driver.manage().window().getSize();
+        int width = dimension.getWidth();
+        int height = dimension.getHeight();
+
+        while (!isPresent(priceDetails)){
+            scrollOrSwipe(width/2, height/2, width/2, 0);
+        }
+
+        String payableAmount = totalPayable.getText().substring(1);
+        ConfigReader.setConfigValue("actual.amount",payableAmount);
+        checkForCouponBtn.click();
     }
 
+    @FindBy(xpath = "//android.widget.TextView/preceding-sibling::android.widget.TextView[@text='Apply']")
+    WebElement applyBtn;
+    @FindBy(id = "com.tul.tatacliq:id/txtSecondTermValue")
+    WebElement couponPrice;
+    @FindBy(className = "android.widget.ImageButton")
+    WebElement backBtn;
     @Override
     public void applyCoupon() {
-
+        if(isDisplayed(applyBtn)){
+            double amount = Double.parseDouble(couponPrice.getText().substring(1));
+            ConfigReader.setConfigValue("coupon.amount", String.valueOf(amount));
+            applyBtn.click();
+        }
+        else{
+            backBtn.click();
+        }
     }
 
+    @FindBy(xpath = "//android.view.View[@resource-id='com.tul.tatacliq:id/hurrayThanksView']")
+    WebElement successMsg;
+    @FindBy(xpath = "//android.widget.TextView[@text='Total Payable']/following-sibling::android.widget.TextView")
+    WebElement totalPrice;
     @Override
     public boolean isCouponApplied() {
-        return false;
+        if(isDisplayed(successMsg)){
+            double total = Double.parseDouble(totalPrice.getText().substring(1));
+            return successMsg.isDisplayed() && total == Double.parseDouble(
+                    ConfigReader.getConfigValue("actual.amount")) -
+                    Double.parseDouble(ConfigReader.getConfigValue("coupon.amount"));
+        }
+        return true;
     }
 }
